c7af70a648b891b7ad94a6528e270c7cf77d8793
Hady Baldé <hadybalde05@gmail.com>
2026-06-03 13:03:27 +0200

    fix: update default port from 8080 to 3001

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ app.use("/tasks", taskRoutes);
 // Error Handler
 app.use(errorHandler);
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3001;
 
 app.listen(port, () =>
   console.log(`app listening on http://localhost:${port}`)
